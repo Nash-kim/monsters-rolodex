863d2c55c7592d33c19c5c9b9d2f3862295ed592
Nash <nash@gmail.com>
2019-10-10 09:16:28 +0800

    Updates

---
+++ precache-manifest.02e0d0a93e48c4d77dff73ab50880e4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad76f17e12d4820b4fbdce517bbf93e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "829b9213d276ae33cd54",
+    "url": "/monsters-rolodex/static/css/main.2b768cdb.chunk.css"
+  },
+  {
+    "revision": "c03d3f64331e577d1529",
+    "url": "/monsters-rolodex/static/js/2.b04a52b1.chunk.js"
+  },
+  {
+    "revision": "829b9213d276ae33cd54",
+    "url": "/monsters-rolodex/static/js/main.36ad2cd2.chunk.js"
+  },
+  {
+    "revision": "bee225b9d88e42814c41",
+    "url": "/monsters-rolodex/static/js/runtime-main.55ecfcda.js"
+  }
+]);
